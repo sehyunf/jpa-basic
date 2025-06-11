316f2b1a13dcd520e7db6a6ada540d638899c0af
sehyunf <ssam010925@gmail.com>
2025-06-11 10:33:49 +0900

    feat: JPA repository 구성

---
+++ src/main/java/com/app/basic/repository/MemberRepository.java
@@ -0,0 +1,40 @@
+package com.app.basic.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.lang.reflect.Member;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+//    데이터 추가
+    public void save(Member member) {
+        entityManager.persist(member);
+    }
+
+//    1개 유저 조회
+    public Optional<Member> findById(Long id) {
+        Member member = entityManager.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+//    전체 유저 조회
+    public List<Member> findAll() {
+        return entityManager.createQuery("SELECT m FROM Member m", Member.class).getResultList();
+    }
+
+//    update - 변경감지(더티 체크)
+
+//    삭제
+    public void delete(Member member) {
+        entityManager.remove(member);
+    }
+
+}
